4bd60ab83a7450f06572ced72754dc34d1304cf9
Md. Jobayer Al Madmud <jtuser112@gmail.com>
2026-01-15 20:45:32 +0600

    (update) refactored proprety dto class for summary

--- src/main/java/org/booking/Property/PropertyDto.java
+++ src/main/java/org/booking/Property/PropertyDto.java
@@ -1,8 +1,17 @@
 package org.booking.Property;
 
 import lombok.Data;
+import org.booking.Apartment.ApartmentDto;
+
+import java.math.BigDecimal;
+import java.util.Set;
 
 @Data
 public class PropertyDto {
-
+    private Long id;
+    private String name;
+    private String address;
+    private BigDecimal latitude;
+    private BigDecimal longitude;
+    private Set<ApartmentDto> apartments;
 }
